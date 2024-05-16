e7d5ac05c16883ee08ecd551f23bd4262b4cfab9
Raí <raivitor@gmail.com>
2024-05-16 18:19:01 -0300

    2 exercícios

---
+++ sequencial/01.js
@@ -0,0 +1,10 @@
+// Faça um programa que receba quatro números inteiros, calcule e mostre a soma desses números.
+let num1 = 10
+let num2 = 20
+let num3 = 30
+let num4 = 40
+console.log(num1, num2, num3, num4)
+
+const soma = num1 + num2 + num3 + num4
+
+console.log(soma)
---
+++ sequencial/02.js
@@ -0,0 +1,20 @@
+// Faça um programa que receba três notas, calcule e mostre a média aritmética.
+
+/*
+let nota1 = 10
+let nota2 = 9.5
+let nota3 = 9
+*/
+
+let nota1
+let nota2
+let nota3
+
+nota1 = 10
+nota2 = 5
+nota3 = 5.8
+
+let soma = nota1 + nota2 + nota3
+//console.log(soma)
+let media = soma / 3
+console.log(media)
